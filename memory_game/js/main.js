bd28492ec6c8e1d6b2746299414ac13810fbe28a
vigbit <vigneshb@live.com>
2020-03-13 18:43:02 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,27 @@
 
-const cards = ["queen", "queen", "king", "king"];
+const cards = [
+{
+    rank : 'queen',
+    suit : 'hearts',
+    cardImage : 'images/queen-of-hearts.png',
+},
+{
+    rank : 'queen',
+    suit : 'diamonds',
+    cardImage : 'images/queen-of-diamonds.png',
+},
+{
+    rank : 'king',
+    suit : 'hearts',
+    cardImage : 'images/king-of-hearts.png',
+},
+{
+    rank : 'king',
+    suit : 'diamonds',
+    cardImage : 'images/king-of-diamonds.png',
+}
+
+];
 
 const cardsInPlay = [];
 
@@ -15,10 +37,13 @@ function flipCard(cardId) {
 if (cardsInPlay.length === 2) {
 checkForMatch();
 	}
-console.log("user flipped " + cards[cardId]);
-cardsInPlay.push(cards[cardId]);
+console.log("user flipped " + cards[cardId].rank);
+cardsInPlay.push(cards[cardId].rank);
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit);
 }
 
 
 flipCard(0) ;
 flipCard(2) ;
+checkForMatch();
