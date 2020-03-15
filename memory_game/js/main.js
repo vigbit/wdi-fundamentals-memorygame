39eb2b010f0ea0172280fdc9b50514822740ec1e
vigbit <vigneshb@live.com>
2020-03-15 18:19:49 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -4,6 +4,7 @@ const cards = [
     rank : 'queen',
     suit : 'hearts',
     cardImage : 'images/queen-of-hearts.png',
+
 },
 {
     rank : 'queen',
@@ -33,17 +34,30 @@ function checkForMatch() {
 	}
 }
 
-function flipCard(cardId) {
-if (cardsInPlay.length === 2) {
-checkForMatch();
-	}
-console.log("user flipped " + cards[cardId].rank);
-cardsInPlay.push(cards[cardId].rank);
-console.log(cards[cardId].cardImage);
-console.log(cards[cardId].suit);
-}
+function flipCard() {
+    let cardId = this.getAttribute('data-id');
+    this.setAttribute('src', cards[cardId].cardImage);
+
+    if (cardsInPlay.length === 2) {
+    checkForMatch();
+    	}
+    console.log("user flipped " + cards[cardId].rank);
+    cardsInPlay.push(cards[cardId].rank);
+    console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].suit);
+    }
 
 
-flipCard(0) ;
-flipCard(2) ;
-checkForMatch();
+function createBoard() {
+    for (let i = 0; i < cards.length; i ++ ) {
+        let cardElement = document.createElement('img');
+        cardElement.setAttribute('src', 'images/back.png');
+        cardElement.setAttribute('data-id', i);
+        cardElement.addEventListener('click', flipCard);
+        document.querySelector('#game-board').appendChild(cardElement);
+
+
+    }
+    }
+
+createBoard();
